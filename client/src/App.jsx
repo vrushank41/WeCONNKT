e83bfbefa5d65cb2c8e57e6e007e2e959eaa932c
Vrushank G <vrushankno2@gmail.com>
2022-10-28 18:47:20 +0530

    Added chatinp and welcome

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,11 @@ import './stylesheets/App.css'
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Outlet, Link } from "react-router-dom";
+
 import Login from "./pages/Login"
 import Register from './pages/Register';
+import ChatInput from './components/chatinput';
+import Welcome from './components/welcome';
 
 
 function App() {
@@ -14,6 +17,8 @@ function App() {
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/chat-input' element={<ChatInput/>} />
+          <Route path='/welcome' element={<Welcome/>} />
         </Routes>
       </Router>
 
