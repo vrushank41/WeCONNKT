4f8c50bde32b68b25f81ac5cccd9b98e7442590d
Vrushank G <vrushankno2@gmail.com>
2022-10-28 16:43:08 +0530

    Added Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import './stylesheets/App.css'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import Login from "./pages/Login"
 
 function App() {
   return (
-    <div className="App">
-      Welcome to WeConnkt😀😀😀😀
+    <div className="App">   
+
+    <BrowserRouter>
+    <Routes>
+        <Route path='/login' element={<Login/>}/>       
+    </Routes>
+    </BrowserRouter>
+
+     
     </div>
   )
 }
